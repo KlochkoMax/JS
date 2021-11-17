18360f2d2bf3f5edc1b7fb2e21a4696c7bdd66c0
MaxymKlochko <klo4komax@gmail.com>
2021-11-17 23:22:37 +0300

    added homework & classwork for lesson 3

---
+++ lesson4/homework/main.js
@@ -0,0 +1,122 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+    // function rectangle (a, b){
+    //     let result = a * b;
+    //     return result;
+    // }
+    // console.log(rectangle(10,35));
+    // document.write('Площа прямокутника - '+rectangle(10,35)+ `<br>`);
+    //
+    // let answer = rectangle(10,35);
+    // console.log(answer);
+    // document.write('Площа прямокутника - '+answer);
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+    // function circle (r){
+    //         let result = 3.14 * r**2;
+    //         return result;
+    // }
+    // console.log(circle(10));
+    // document.write('Площа круга - ' + circle(10) + `<br>` )
+    //
+    // let answer = circle(10)
+    // console.log(answer);
+    // document.write('Площа круга - '+answer);
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+    // function cilinder (r,h){
+    //         let result = 3.14 * r**2 * h;
+    //         return result;
+    // }
+    // console.log(cilinder(10,5));
+    // document.write('Площа циліндра - ' + cilinder(10,5) + `<br>` );
+    //
+    // let answer = cilinder(10, 5);
+    // console.log(answer);
+    // document.write('Площа циліндра - '+answer);
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+//     let masive = [ 'a', 'b', 'c', 1, 2, 3, true, false];
+//     function massive1 (masive){
+//         for ( i = 0; i < masive.length; i++){
+//             console.log(masive[i]);
+//         }
+//     }
+//
+//     console.log(massive1(masive));
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+//         function paragraph (a){
+//             document.write(`<p>${a}</p>`);
+//
+//         }
+//
+//         paragraph("Якийсь текст")
+//         paragraph('Якийсь інший текст')
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+        // function list (a){
+        //     document.write(`<ul>`);
+        //     document.write(`<li>${a}</li>`);
+        //     document.write(`<li>${a}</li>`);
+        //     document.write(`<li>${a}</li>`);
+        //     document.write(`</ul>`);
+        // }
+        //
+        // list('item')
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+//     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+//         function list (size,a){
+//             document.write(`<ul>`);
+//             for (let i=0; i < size; i++){
+//                 document.write(`<li>${a}</li>`);
+//             }
+//             document.write(`</ul>`);
+//         }
+//
+//         list(5,"text")
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+        // let anothermasive = ['someword', 1, true, 'another', 25];
+        // let masive = [ 1,2,'hate', 'js', true,false];
+        // function list (somemassive){
+        //     document.write(`<ul>`)
+        //         for (let i = 0; i <somemassive.length; i++ ){
+        //             document.write(`<li> ${somemassive[i]} </li>`)
+        //         }
+        //     document.write(`</ul>`)
+        // }
+        //
+        // list(masive)
+        // list(anothermasive)
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.' +
+// ' Для кожного об'єкту окремий блок.
+
+    // let masive = [{id: 1, name : 'Bart', age: 16} ,
+    //               {id: 2, name : 'Gomer', age: 46},
+    //               {id: 3, name : 'Marge', age: 42}];
+    //
+    // function list (arraymasive){
+    //     document.write(`<div>`)
+    //     for ( let i = 0; i < arraymasive.length; i++ ){
+    //         let array = arraymasive[i]
+    //         document.write(`<div>${array.id}. Name- ${array.name}, age - ${array.age} `)
+    //     }
+    //     document.write(`</div>`)
+    //
+    // }
+    //
+    // list(masive)
+
+
+
+
+
+
+
