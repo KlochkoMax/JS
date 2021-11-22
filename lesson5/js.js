65dde99661a143b50f1228e633c8745c05ad71ab
MaxymKlochko <klo4komax@gmail.com>
2021-11-22 22:24:39 +0300

    added homework for lesson 5

---
+++ lesson5/js.js
@@ -0,0 +1,89 @@
+// Всі функції повинні бути описані стрілочним типом!!!!
+//     - створити функцію яка обчислює та повертає площу прямокутника висотою
+
+    // calc = (a,b) => a * b;
+    // console.log(calc(5,15));
+
+// - створити функцію яка обчислює та повертає площу кола
+
+    // calc = (r) => 3.14 * r**2;
+    // console.log(calc(5));
+
+// - створити функцію яка обчислює та повертає площу циліндру
+
+    // calc = (h,r) => 2 * 3.14 * r * ( h + r);
+    // console.log(calc(5,8));
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+    // let array = [1, 2, 3, true, 'hello'];
+    // let array1 = ['+', 44, true, 22];
+    //
+    // func = (a) => {
+    //     for (let i = 0; i < a.length; i++) {
+    //         console.log(a[i])
+    //     }
+    //
+    // }
+    // func(array);
+    // func(array1);
+
+// - створити функцію яка  створює параграф з текстом. Текст задати через аргумент
+
+    // func = (text) => document.write(`<p>${text}</p>`)
+    //
+    // func('Hello');
+    // func('Okten');
+
+
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+//     func = (text) => {
+//         document.write(`<ul>`);
+//         document.write(`<li>${text}</li>`);
+//         document.write(`<li>${text}</li>`);
+//         document.write(`<li>${text}</li>`);
+//         document.write(`</ul>`);
+//     }
+//
+//     func('hello')
+
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+//     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+    // func = (s,t) => {
+    //     document.write(`<ul>`);
+    //     for (let i = 0; i < s; i++) {
+    //         document.write(`<li>${t}</li>`);
+    //     }
+    //     document.write(`</ul>`);
+    // }
+    // func(5,'Hello')
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+    // func = (list) => {
+    //     document.write(`<ul>`)
+    //     for (let i = 0; i < list.length; i++) {
+    //         document.write(`<li>${list[i]}</li>`)
+    //     }
+    // }
+    //
+    // let array = [1,2,true, false, 'Hello']
+    // func(array);
+
+// створити функцію яка приймає масив об'єктів з наступними полями id,name,age ,' +
+//  та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+    // func = (list) => {
+    //     for (let i = 0; i < list.length; i++) {
+    //         const listElement = list[i];
+    //         document.write(`<div>${listElement.id}. Name - ${listElement.name}. Age - ${listElement.age}</div> `);
+    //     }
+    // }
+    //
+    //     let masive = [{id: 1, name : 'Vasyl', age: 26} ,
+    //                   {id: 2, name : 'Ivan', age: 33},
+    //                   {id: 3, name : 'Mariya', age: 11}];
+    //
+    // func(masive);
+
